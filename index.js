a9ad423f063b4bdb3122d294ba00b28ace988df6
Andy <andy.lee.surfer@gmail.com>
2017-02-13 11:08:27 +0000

    Basic Azure support

---
+++ index.js
@@ -0,0 +1,5 @@
+﻿const Server = require('zwift-second-screen/server/server');
+const Login = require('zwift-second-screen/server/login');
+
+const server = new Server(new Login());
+server.start(process.env.PORT || 3000);
