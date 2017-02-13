c8226b09a9cce2789d569bdf5aa2b4d47ae23c2a
Andy <andy.lee.surfer@gmail.com>
2017-02-13 15:51:21 +0000

    Added settings for maps

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 ﻿const Server = require('zwift-second-screen/server/server');
 const Login = require('zwift-second-screen/server/login');
-const settings = require('zwift-second-screen/settings');
+const settings = require('./settings');
 
 const server = new Server(new Login(), { worlds: settings.worlds });
 server.start(process.env.PORT || 3000);
