c92891f15fada4b1bcf953b4ff85cccd24e70ff5
Andy <andy.lee.surfer@gmail.com>
2017-02-15 14:51:33 +0000

    Support for signing in with rider id

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 ﻿const Server = require('zwift-second-screen/server/server');
 const Login = require('zwift-second-screen/server/login');
+const RiderId = require('zwift-second-screen/server/riderId');
 const settings = require('./settings');
 
-const server = new Server(new Login(), { worlds: settings.worlds });
+const username = process.env.ServiceUsername;
+const password = process.env.ServicePassword;
+
+const riderProvider = (username && password)
+      ? new RiderId(username, password)
+      : new Login();
+
+const server = new Server(riderProvider, { worlds: settings.worlds });
 server.start(process.env.PORT || 3000);
