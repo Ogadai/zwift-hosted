8078898175cceccdaf093bfbbe13c8ef596c91d9
Andy <andy.lee.surfer@gmail.com>
2017-02-28 12:02:58 +0000

    Added analytics support

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const settings = require('./settings');
 const username = process.env.ServiceUsername;
 const password = process.env.ServicePassword;
 
+const trackingId = process.env.TrackingId;
+const analytics = trackingId ? { trackingId } : undefined;
+
 const riderProvider = (username && password)
       ? new RiderId(username, password)
       : new Login();
 
-const server = new Server(riderProvider, { worlds: settings.worlds });
+const server = new Server(riderProvider, { worlds: settings.worlds, analytics });
 server.start(process.env.PORT || 3000);
