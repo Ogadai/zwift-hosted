e55348b4a8f49d23f911ba09676405b31d3f2489
Andy <andy.lee.surfer@gmail.com>
2017-11-03 12:39:03 +0000

    Update Watopia map and world-id fixes

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@
     1: {
       map: '/maps/watopia.png',
       background: '#DBEFFC',
-      viewBox: '-551868 -323969 599071 900906',
+      viewBox: '-617368 -435969 776071 900906',
       credit: { prompt: 'Powered by', name: 'ZwiftBlog', href: 'http://zwiftblog.com/' }
     },
     2: {
