ae5bff9a4896fd60e74cf8e4870d4fecf88187a6
Andy <andy.lee.surfer@gmail.com>
2019-04-22 12:21:45 +0100

    Update Watopia

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ module.exports = {
       map: '/maps/watopia.png',
       roads: '/maps/watopia-roads.json',
       background: '#0886E4',
-      viewBox: '-641000 -801000 1270000 847000',
+      viewBox: '-641000 -801000 1640500 794000',
       rotate: "(-90,122141,234864)",
       translate: "(949427,295876)",
       credit: { prompt: 'Powered by', name: 'ZwiftBlog', href: 'http://zwiftblog.com/' },
