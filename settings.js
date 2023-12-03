c7770a9d2068dea882c779e06fb0d7f7020ff878
MartinWTRL <martincarew@hotmail.com>
2023-12-03 16:20:41 -0700

    viewbox tweaks

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ module.exports = {
             map: '/maps/watopia.jpg',
             roads: '/maps/watopia-roads.json',
             background: '#0886E4',
-            viewBox: '-973000 -836500 2111300 1147400',
+            viewBox: '-972000 -835500 2111300 1147400',
             rotate: "(-90,122141,234864)",
             translate: "(949427,295876)",
             credit: { prompt: 'Powered by', name: 'WTRL Racing', href: 'https://www.wtrl.racing' },
