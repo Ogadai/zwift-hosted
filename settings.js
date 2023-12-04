59120e2b061db99b91d63a8ad773debdb77d6964
MartinWTRL <martincarew@hotmail.com>
2023-12-03 23:14:14 -0700

    rollback to viewbox

--- settings.js
+++ settings.js
@@ -7,23 +7,23 @@ module.exports = {
             map: '/maps/watopia.jpg',
             roads: '/maps/watopia-roads.json',
             background: '#0886E4',
-            viewBox: '-836000 -865500 1822300 1341400',
+            viewBox: '-972000 -835500 1822300 1341400',
             rotate: "(-90,122141,234864)",
             translate: "(949427,295876)",
             credit: { prompt: 'Powered by', name: 'WTRL Racing', href: 'https://www.wtrl.racing' },
             points: {
                 get: () => Promise.resolve([
-                    { name: 'Start Banner', x: 107500, y: -10000, image: 'start', rotate: 160 },
-                    { name: 'Hilly Fwd Start', x: 46683, y: 72712, image: 'climbstart', rotate: 60 },
-                    { name: 'Hilly KOM', x: 46683, y: 72712, image: 'kom', rotate: 60 },
-                    { name: 'Hilly Rev Start', x: 46683, y: 72712, image: 'climbstart', rotate: 60 },
+                    { name: 'Start Banner', x: 107500, y: -10000, image: 'start', rotate: 170 },
+                   /* { name: 'Hilly Fwd Start', x: 49683, y: 54712, image: 'climbstart', rotate: 60 },*/
+                    { name: 'Hilly KOM', x: 49683, y: 54712, image: 'kom', rotate: 60 },
+                    /*{ name: 'Hilly Rev Start', x: 49683, y: 54712, image: 'climbstart', rotate: 60 },*/
                     /*{ name: 'JWB Fwd Start', x: -24300, y: -40500, image: 'sprintstart', rotate: 60 },*/
-                    { name: 'JWB Sprint', x: -41300, y: -39500, image: 'sprint', rotate: 60 },
+                    { name: 'JWB Sprint', x: -24300, y: -40500, image: 'sprint', rotate: 60 },
                     /*{ name: 'JWB Rev Start', x: -24300, y: -40500, image: 'sprintstart', rotate: 60 },
                     { name: 'Epic Fwd Start', x: -361987, y: 86048, image: 'climbstart', rotate: 30 },*/
-                    { name: 'Fuego Sprint', x: 59850, y: 390000, image: 'start', rotate: 20 },
-                    { name: 'Epic KOM', x: -440987, y: 115048, image: 'mountain', rotate: 50 },
-                    { name: 'Jungle Start', x: -598690, y: -295679, image: 'start', rotate: 20 },
+                    { name: 'Epic KOM', x: -361987, y: 86048, image: 'mountain', rotate: 30 },
+                    { name: 'Jungle Start', x: -498690, y: -268679, image: 'start', rotate: 180 },
+                    { name: 'Fuego Sprint', x: 62256, y: 321485, image: 'sprint', rotate: 170 },
                     /*{ name: 'Epic Rev Start', x: -361987, y: 86048, image: 'climbstart', rotate: 30 },
                     { name: 'Jungle Start', x: -499228, y: -263780, image: 'banner', rotate: 20 },
                     { name: 'Volcano Start', x: -209968.36, y: -55463.453, image: 'climbstart', rotate: 15 },
@@ -36,12 +36,12 @@ module.exports = {
                     { name: 'Woodland Fwd Start', x: 62256, y: 321485, image: 'sprintstart', rotate: 170 },
                     { name: 'Woodland Sprint', x: 62256, y: 321485, image: 'sprint', rotate: 170 },
                     { name: 'Woodland Rev Start', x: 62256, y: 321485, image: 'sprintstart', rotate: 170 },
-                    { name: 'Sasquatch Fwd Start', x: -227425.2, y: 65148.137, image: 'sprintstart', rotate: 15 },*/
-                    { name: 'Sasquatch Sprint', x: -593821.88, y: 533120.25, image: 'sprint', rotate: 80 },
-                    /*{ name: 'Sasquatch Rev Start', x: -227425.2, y: 65148.137, image: 'sprintstart', rotate: 15 }
                     { name: 'Acropolis Fwd Start', x: -227425.2, y: 65148.137, image: 'sprintstart', rotate: 15 },*/
-                    { name: 'Acropolis Sprint', x: -859499.1, y: -23002, image: 'sprint', rotate: 70 },
-                    /*{ name: 'Acropolis Rev Start', x: -227425.2, y: 65148.137, image: 'sprintstart', rotate: 15 },*/
+                    { name: 'Acropolis Sprint', x: -727499.1, y: 25671.162, image: 'sprint', rotate: 90 },
+                    /*{ name: 'Acropolis Rev Start', x: -227425.2, y: 65148.137, image: 'sprintstart', rotate: 15 },
+                    { name: 'Sasquatch Fwd Start', x: -227425.2, y: 65148.137, image: 'sprintstart', rotate: 15 },*/
+                    { name: 'Sasquatch Sprint', x: -497821.88, y: 448120.25, image: 'sprint', rotate: 80 },
+                    /*{ name: 'Sasquatch Rev Start', x: -227425.2, y: 65148.137, image: 'sprintstart', rotate: 15 },*/
                 ])
             }
         },
